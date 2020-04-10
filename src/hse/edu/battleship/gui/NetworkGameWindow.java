2fea1fae136f65d0c7106d8c61e7bb69bb465938
Armen Matevosyan <aamatevosyan@edu.hse.ru>
2020-04-10 14:10:46 +0400

    Changed structure, added comments

--- src/hse/edu/battleship/gui/NetworkGameWindow.java
+++
@@ -1,9 +0,0 @@
-package hse.edu.battleship.gui;
-
-import hse.edu.battleship.core.Ocean;
-
-public class NetworkGameWindow extends GameWindow {
-    public NetworkGameWindow(Ocean ocean) {
-        super(ocean);
-    }
-}
